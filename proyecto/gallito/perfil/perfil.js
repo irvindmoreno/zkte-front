7c1fa0d73f309aa9d9b263bf429ef30d8a522073
Irvin DANIEL <irvind.moreno@gmail.com>
2016-08-08 20:44:10 -0500

    proyecto

---
+++ proyecto/gallito/perfil/perfil.js
@@ -0,0 +1,9 @@
+$(document).on("ready",inicio)
+function inicio()
+{	
+    var headerGallitoDashboard= new HeaderGallitoDashboard()
+    var filtroGallito= new FiltroGallito()
+    var iconsGallitoPerfil= new IconsGallitoPerfil()
+    var formularioGallito= new FormularioGallito()
+    var footerGallito= new FooterGallito()
+}
---
+++ public/proyecto/gallito/perfil/perfil.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$(document).on("ready", inicio);
+function inicio() {
+    var headerGallitoDashboard = new HeaderGallitoDashboard();
+    var filtroGallito = new FiltroGallito();
+    var iconsGallitoPerfil = new IconsGallitoPerfil();
+    var formularioGallito = new FormularioGallito();
+    var footerGallito = new FooterGallito();
+}
